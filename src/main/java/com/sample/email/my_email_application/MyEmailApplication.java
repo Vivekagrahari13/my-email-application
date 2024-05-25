fd9ab9fb0409b9c793399b5f3499e21724071942
Vivek Agrahari <vivek.agrahari@xcelore.com>
2024-05-25 11:54:36 +0530

    Initial commit

---
+++ src/main/java/com/sample/email/my_email_application/MyEmailApplication.java
@@ -0,0 +1,13 @@
+package com.sample.email.my_email_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/email/my_email_application/MyEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.email.my_email_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
